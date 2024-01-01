3d0f4aa31be9c7f79f749c1d1e072905d40437a2
Bruno Souza <brunoezully@gmail.com>
2024-01-01 19:43:14 -0300

    feat: implementação da` api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;")
-  console.log(result.rows)
-  response.status(200).json({
-    "message": "Comunicação com a api feita com sucesso"
-  })
+  
+    const updatedAt = new Date().toISOString();
+    const databaseVersion = await database.query("SHOW server_version;");
+    const databaseMaxConnections = await database.query("SHOW max_connections;");
+
+    const databaseName = process.env.POSTGRES_DB;
+    const databaseCurrentConnections = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+    const databaseConnectionsValue = databaseCurrentConnections.rows.length
+    console.log(databaseCurrentConnections.rows[0].count)
+  
+    response.status(200).json({
+      updated_at: updatedAt,
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(databaseMaxConnections.rows[0].max_connections),
+        opened_connections: databaseConnectionsValue,
+      }
+    });
 }
 
 export default status;
--- test/integration/get.test.js
+++ test/integration/get.test.js
@@ -1,4 +1,15 @@
 test("Get to /api/v1/status should return 200", async () =>{
   const response = await fetch("http://localhost:3000/api/v1/status")
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.database).toBeDefined();
+  expect(responseBody.database.version).toBe("16.0");
+  expect(responseBody.database.max_connections).toEqual(100);
+  expect(responseBody.database.opened_connections).toBe(1);
+
 });
